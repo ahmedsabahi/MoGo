8658267984ea0f77d2a0a826c399da0baa496eaf
Ahmed Sabahi <a7md2shrf@gmail.com>
2021-03-18 14:41:06 +0200

    add features

--- js/temp.js
+++ js/temp.js
@@ -1,25 +1,42 @@
-jQuery(function ($) {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 200) {
-      $("#scrollToTop ").fadeIn();
-    } else {
-      $("#scrollToTop ").fadeOut();
-    }
-  });
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 200) {
+    $("#scrollToTop ").fadeIn();
+  } else {
+    $("#scrollToTop ").fadeOut();
+  }
+});
 
-  $("#scrollToTop ").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 800);
-    return false;
-  });
+$("#scrollToTop ").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 800);
+  return false;
+});
 
-  $(window).scroll(function () {
-    if (
-      $(this).scrollTop() == $("#header").height() ||
-      $(this).scrollTop() > $("#header").height()
-    ) {
-      $("body").addClass("fixed-nav");
-    } else {
-      $("body").removeClass("fixed-nav");
-    }
-  });
+$("#navbar a").on("click", function (e) {
+  e.preventDefault();
+  var target = this.hash,
+    $target = $(target);
+
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $target.offset().top - 20,
+      },
+      500,
+      "swing",
+      function () {
+        window.location.hash = target;
+      }
+    );
+});
+
+$(window).scroll(function () {
+  if (
+    $(this).scrollTop() == $("#header").height() ||
+    $(this).scrollTop() > $("#header").height()
+  ) {
+    $("body").addClass("fixed-nav");
+  } else {
+    $("body").removeClass("fixed-nav");
+  }
 });
